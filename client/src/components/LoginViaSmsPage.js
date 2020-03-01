57f865b23b100a066deff7ec247dc901a0e55127
shitot handasi <shitotazure@gmail.com>
2020-03-01 19:20:53 +0200

    with sms

--- client/src/components/LoginViaSmsPage.js
+++ client/src/components/LoginViaSmsPage.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {BASE_URL} from '../constants'
+import axios from 'axios'
+// import logo from '../images/Zofim-logo.png';
+import LoadingPage from './LoadingPage';
 
-const LoginViaSmsPage = () => (
-    <div className="container">
-        <h2>התחברות לאזור אישי באמצעות סמס</h2>
-        <div className="sub-container">
-            <p>תעודת זהות *</p>
-            <input />
-            <p>טלפון נייד *</p>
-            <form className="code-input">
-                <input />
-                <button disabled={false}>קבלת קוד</button>
-            </form>
-            <p>קוד *</p>
-            <input 
-                placeholder="יש להזין את הקוד שקיבלת"
-                disabled={true}
-            />
-            
-            <button 
-                className="login-button"
-                disabled={true}
-            >
-                כניסה
-            </button>
-        </div>
-    </div>
-);
+const LoginViaSmsPage = () => {
+    const [phone,setPhone] = useState('')
+    const [id,setId] = useState('')
+    const [press,setPress] = useState(false)
+    const [loader,setLoader] = useState(false)
+
+    if(loader){
+        return(
+            <LoadingPage/>
+        )
+    }
+    else{
+        return(
+            <div className="container">
+                <h2>התחברות לאזור אישי באמצעות סמס</h2>
+                <div className="sub-container">
+                    <p>תעודת זהות *</p>
+                    <input value={id} onChange={(e)=>setId(e.target.value)}/>
+                    <p>טלפון נייד *</p>
+                    <form className="code-input">
+                        <input value={phone} onChange={(e)=>setPhone(e.target.value)} />
+                        <button onClick={()=>{
+                            setPress(!press)
+                            setLoader(true)
+                            axios
+                            .post(BASE_URL+'/api/validation/',{number:phone})
+                            .then(res=>{
+                                console.log(res)
+                                setLoader(false)
+                            })
+                        }} disabled={press}>קבלת קוד</button>
+                    </form>
+                    <p>קוד *</p>
+                    <input 
+                        placeholder="יש להזין את הקוד שקיבלת"
+                        disabled={!press}
+                    />
+                    
+                    <button 
+                        className="login-button"
+                        disabled={!press}
+                    >
+                        כניסה
+                    </button>
+                </div>
+            </div>
+        )
+    }
+};
 
 export default LoginViaSmsPage; 
